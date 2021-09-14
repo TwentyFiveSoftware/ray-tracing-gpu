68b6166b30241d76843c8f18a01709a1838fbbff
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-09-14 11:48:26 +0200

    render time measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "vulkan.h"
+#include <chrono>
+#include <iostream>
 
 int main() {
     VulkanSettings settings = {
@@ -10,8 +12,16 @@ int main() {
 
     Vulkan vulkan(settings);
 
+    auto renderBeginTime = std::chrono::steady_clock::now();
+
+    vulkan.render();
+
+    auto renderTime = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::steady_clock::now() - renderBeginTime).count();
+
+    std::cout << "Rendered in " << (float(renderTime) / 1000.0f) << " ms" << std::endl;
+
     while (!vulkan.shouldExit()) {
         vulkan.update();
-        vulkan.render();
     }
 }
